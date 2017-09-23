8a6e8fea2b08574df8210cfb142edcc4f8dd01b3
Eugene <boberr10@yandex.ru>
2017-09-23 21:23:58 +0300

    change password validator

--- app/validators/password_validator.rb
+++ app/validators/password_validator.rb
@@ -1,4 +1,6 @@
 class PasswordValidator < ActiveModel::Validator
+  I18N_SCOPE = :validators.freeze
+
   VALIDATIONS = [
     { type: 'uppercase', regexp: /[A-Z]/ },
     { type: 'lowercase', regexp: /[a-z]/ },
@@ -16,15 +18,18 @@ class PasswordValidator < ActiveModel::Validator
 
   def validate_spaces(record)
     return unless record.password&.include?(' ')
-    record.errors.add(:password, "mustn't contain spaces")
+    record.errors.add(
+      :password,
+      I18n.t(:spaces, scope: I18N_SCOPE)
+    )
   end
 
   VALIDATIONS.each do |validation|
     define_method "validate_#{validation[:type]}" do |record|
-      return if record.password.match?(validation[:regexp])
+      return if record.password&.match?(validation[:regexp])
       record.errors.add(
         :password,
-        "must contain at least 1 #{validation[:type]}"
+        I18n.t(:contain, scope: I18N_SCOPE, type: validation[:type])
       )
     end
   end
