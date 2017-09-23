2c04104a79f57ba66deeabb290bab4bf732ed9b8
Eugene <boberr10@yandex.ru>
2017-09-23 23:11:36 +0300

    add user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,10 @@
-RSpec.describe User, type: :model do
-  # subject(:user) { build(:user) }
-  
+RSpec.describe User, type: :model do 
   context 'associations' do
-    %i[reviews orders].each do |association_name|
-      it { is_expected.to have_many(association_name) }
-    end
+    it { is_expected.to have_many(:reviews).dependent(:destroy) }
+    it { is_expected.to have_many(:orders).dependent(:nullify) }
 
     %i[shipping_address billing_address].each do |association_name|
-      it { is_expected.to have_one(association_name) }
+      it { is_expected.to have_one(association_name).dependent(:destroy) }
     end
   end
 
@@ -17,9 +14,13 @@ RSpec.describe User, type: :model do
     end
   end
 
-  # context 'validation' do
-  #   it { is_expected.to validate_length_of(:first_name).is_at_most(50) }
-  #   it { is_expected.to validate_length_of(:last_name).is_at_most(50) }
-  #   it { is_expected.to validate_length_of(:email).is_at_most(63) }
-  # end
+  context 'validation' do
+    it { is_expected.to validate_length_of(:password).is_at_least(8) }
+    it { is_expected.not_to allow_value('qwertyui').for(:password) }
+    it { is_expected.not_to allow_value('QWERTYUI').for(:password) }
+    it { is_expected.not_to allow_value('12345678').for(:password) }
+    it { is_expected.not_to allow_value('Qwertyui').for(:password) }
+    it { is_expected.not_to allow_value('1wertyui').for(:password) }
+    it { is_expected.to allow_value('Q1ertyui').for(:password) }
+  end
 end
