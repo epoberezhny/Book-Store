4942e4c35d1ee328bac3610b8920762d0ff71b74
Eugene <boberr10@yandex.ru>
2017-10-10 15:56:10 +0300

    update credit card decorator

--- app/decorators/credit_card_decorator.rb
+++ app/decorators/credit_card_decorator.rb
@@ -1,5 +1,8 @@
 class CreditCardDecorator < ApplicationDecorator
+  SHOWED_SYMBOLS = 4
+
   def formatted_number
-    ('*' * 12) << number.slice(12, 15)
+    hidden_symbols = number.length - SHOWED_SYMBOLS
+    ('*' * hidden_symbols) << number[-SHOWED_SYMBOLS, SHOWED_SYMBOLS]
   end
 end
